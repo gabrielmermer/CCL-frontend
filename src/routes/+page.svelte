5bf83c7bd679d70344c878b3c202db2e52a76f63
Gabriel Mermer <zubrzysta@gmail.com>
2023-06-06 20:40:20 +0200

    ver 0.1 r/Polska

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+export let data;
+
+
+
+console.log(data);
+console.log(data.feed.title);
+
+let feed_items = data.feed.items;
+console.log(feed_items);
+
+
+</script>
+
+{#each feed_items as post}
+	<a href="{post.link}" target="_blank">{post.title}</a> <br>
+{/each}
